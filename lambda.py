950d68fd889b6efbcc84bf6019976ec173d542f3
reka193 <reka193@hotmail.com>
2019-06-27 09:20:37 +0200

    comments added

--- lambda.py
+++ lambda.py
@@ -1,12 +1,12 @@
 from telegram.ext import Updater, CommandHandler
-import telegram
 import requests
 import boto3
 from bs4 import BeautifulSoup
 
-token = '889819370:AAEmCPz47xgffA9rPPy6V8NuJee-FzaYscw'
+token = <BOTTOKEN>
 
 
+# Extracting the necessary information from the website
 def get_info():
     page = requests.get('https://www.jma.go.jp/en/quake/')
     bs_page = BeautifulSoup(page.text, 'html.parser')
@@ -16,6 +16,7 @@ def get_info():
     return datetime.text, magnitude.text, region.text
 
 
+# Basic message sender for the 'help' command
 def eq_message(bot, update):
     datetime, magnitude, region = get_info()
     chat_id = update.message.chat_id
@@ -23,6 +24,7 @@ def eq_message(bot, update):
     bot.send_message(chat_id=chat_id, text=text)
 
 
+# Function for the 'add' command, subscribing the user by adding the chat_id to the db
 def subscribe(bot, update):
     chat_id = update.message.chat_id
     dynamodb = boto3.client('dynamodb')
@@ -33,6 +35,7 @@ def subscribe(bot, update):
     bot.send_message(chat_id=chat_id, text="You have subscribed!")
 
 
+# Function for the 'delete' command, unsubscribe the user by deleting her chat_id from the db
 def unsubscribe(bot, update):
     chat_id = update.message.chat_id
     dynamodb = boto3.client('dynamodb')
@@ -43,16 +46,19 @@ def unsubscribe(bot, update):
     bot.send_message(chat_id=chat_id, text="You have unsubscribed!")
 
 
+# send notification to the chat_ids in the db, in case there is a new earthquake
 def send_notification(bot):
+    # collect all chat_ids
     dynamodb = boto3.client('dynamodb')
     ids = dynamodb.scan(TableName='subscribers')
-
+    # collect the actual information
     datetime, magnitude, region = get_info()
     eq_info = datetime+magnitude+region
-
+    # collect the last information sent
     old_info = dynamodb.scan(TableName='earthquake_info')
     old_info = old_info['Items'][0]['info']['S']
 
+    # if the information is new, send a notification to all the chat_ids in the db
     if not old_info == eq_info:
         dynamodb.delete_item(TableName='earthquake_info', Key={'info': {'S': str(old_info)}})
         dynamodb.put_item(TableName='earthquake_info', Item={'info': {'S': str(eq_info)}})
@@ -65,6 +71,7 @@ def send_notification(bot):
                 print(e)
 
 
+# lambda handler for aws
 def lambda_handler(event, context):
 
     updater = Updater(token)
